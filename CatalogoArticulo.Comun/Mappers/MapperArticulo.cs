03e5db0fae648b0da3d3c31cd6d71cf1e84fea59
Kimblag <kimberly.blandong@gmail.com>
2025-10-10 11:31:08 -0300

    Se crea el DTO para listar artículos. Se crea el método para mapear el DTO a un listado de la entidad ArtículoListadoDTO y se documenta la clase Mapper

--- CatalogoArticulo.Comun/Mappers/MapperArticulo.cs
+++ CatalogoArticulo.Comun/Mappers/MapperArticulo.cs
@@ -7,11 +7,21 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using CatalogoArticulos.Dominio.Entidades;
 
-
 namespace CatalogoArticulo.Comun.Mappers
 {
+    /// <summary>
+    /// Esta clase está dedicada a realizar el "mapeo" de entidades a DTOs, ya que el controlador es el único
+    /// que puede conocer los DTOs, la capa Negocio sólo conoce
+    /// entidades, de esta manera podemos cumplir con principios de responsabilidad única y desacoplamiento de 
+    /// las capas: si cambia un DTO sólo se cambia la capa de DTOs y el Mapper.
+    /// </summary>
     public static class MapperArticulo
     {
+        /// <summary>
+        /// Método para mapear el DTO <c>ArticuloGuardarDTO</c> del endpoint Guardar a la entidad <c>Articulo</c>
+        /// </summary>
+        /// <param name="dto">El DTO a convertir.</param>
+        /// <returns>La entidad Articulo</returns>
         public static Articulo ToDominio(ArticuloGuardarDTO dto)
         {
             return new Articulo
@@ -26,6 +36,12 @@ namespace CatalogoArticulo.Comun.Mappers
             };
         }
 
+        /// <summary>
+        /// Método para mapear el DTO <c>ArticuloAgregarImagenesDTO</c> del endpoint 
+        /// Guardar Imágenes a una lista de la entidad <c>Imagen</c>
+        /// </summary>
+        /// <param name="dto">El DTO a convertir.</param>
+        /// <returns>Listado de la entidad Imagen</returns>
         public static List<Imagen> ToDominio(ArticuloAgregarImagenesDTO dto)
         {
             var imagenes = new List<Imagen>();
@@ -34,6 +50,42 @@ namespace CatalogoArticulo.Comun.Mappers
                 imagenes.Add(new Imagen { Url = url });
             }
             return imagenes;
+        }
+
+        /// <summary>
+        /// Método para mapear un listado de la entidad <c>Articulo</c> del endpoint 
+        /// Listar Artículos a una lista del DTO <c>ArticuloListadoDTO</c>
+        /// </summary>
+        /// <param name="dto">El DTO a convertir.</param>
+        /// <returns>Listado de del DTO ArticuloListadoDTO</returns>
+        public static List<ArticuloListadoDTO> ToDominio(List<Articulo> listaArticulos)
+        {
+            List<ArticuloListadoDTO> listadoArticulosDto = new List<ArticuloListadoDTO>();
+            foreach (Articulo articulo in listaArticulos)
+            {
+                List<string> urls = new List<string>();
+                foreach (Imagen imagen in articulo.Imagenes)
+                {
+                    urls.Add(imagen.Url);
+                }
+
+                ArticuloListadoDTO articuloActual = new ArticuloListadoDTO
+                {
+                    IdArticulo = articulo.Id,
+                    Codigo = articulo.Codigo,
+                    Nombre = articulo.Nombre,
+                    Descripcion = articulo.Descripcion,
+                    IdMarca = articulo.Marca.Id,
+                    Marca = articulo.Marca.Descripcion,
+                    IdCategoria = articulo.Categoria.Id,
+                    Categoria = articulo.Categoria.Descripcion,
+                    Precio = articulo.Precio,
+                    UrlImagenes = urls
+                };
+                listadoArticulosDto.Add(articuloActual);
+            }
+
+            return listadoArticulosDto;
         }
     }
 }
---
+++ CatalogoArticulos.DTOs/ArticuloListadoDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogoArticulos.DTOs
+{
+    public class ArticuloListadoDTO
+    {
+        public int IdArticulo { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public int IdMarca { get; set; }
+        public string Marca { get; set; }
+        public int IdCategoria { get; set; }
+        public string Categoria { get; set; }
+        public decimal? Precio { get; set; }
+        public List<string> UrlImagenes { get; set; }
+    }
+}
